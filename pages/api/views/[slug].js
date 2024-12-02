0ab2c56a7f5def69fd5d016228c2207aac2686a2
julycrystal <131466754+julycrystal@users.noreply.github.com>
2024-12-02 09:11:59 +0000

    Update front page styles

--- pages/api/views/[slug].js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const slug = req.query.slug.toString()
-    return res.status(200).json({ total: 0 })
-  } catch (e) {
-    return res.status(500).json({ message: e.message })
-  }
-}
