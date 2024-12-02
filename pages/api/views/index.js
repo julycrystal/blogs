8be696a5e3257c8096c60e5fc3504268ec89f928
julycrystal <131466754+julycrystal@users.noreply.github.com>
2024-12-02 02:53:57 +0000

    Remove guestbook

--- pages/api/views/index.js
+++ pages/api/views/index.js
@@ -1,14 +1,6 @@
-import prisma from 'lib/prisma'
-
 export default async function handler(req, res) {
   try {
-    const totalViews = await prisma.views.aggregate({
-      _sum: {
-        count: true,
-      },
-    })
-
-    return res.status(200).json({ total: totalViews._sum.count.toString() })
+    return res.status(200).json({ total: 0 })
   } catch (e) {
     return res.status(500).json({ message: e.message })
   }
